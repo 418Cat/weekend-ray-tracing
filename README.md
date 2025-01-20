d7e569edc102b0060aa175e1b65197de29ee3f84
418Cat <94795008+418Cat@users.noreply.github.com>
2025-01-20 20:46:50 +0100

    Final scene + images & binary
    Added final scene rendering in main.cpp with a for-loop to spawn a bunch of spheres
    Also added the images and binary files
    Edited README

--- README.md
+++ README.md
@@ -3,4 +3,13 @@ I've been meaning to follow this book for a bit now, so here I am.
 
 The book in question is the well known [Ray Tracing in one weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) book by  **Peter Shirley**, **Trevor David Black** and **Steve Hollasch**.
 
-The code isn't an exact copy of theirs, I'll code in my style with some of my ideas and understandings, but the ideas are basically the same.
+The code isn't an exact copy of theirs, I coded in my style with some of my ideas and understandings, but the ideas are basically the same.
+<br>
+___
+<br>
+
+The images in the `./img` directory were created at different steps as I was progressing through the program. They are alphabetically ordered from oldest to most recent.
+<br>
+Here's the final render:
+<img src="https://github.com/418Cat/weekend-ray-tracing/blob/main/img/final_render.png?raw=true">
+<br>
--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,73 @@ int main(int argc, char* argv[])
 {
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<metal>(color(0.1, 0.2, 0.5), 0.05);
-    auto material_left   = make_shared<dielectric>(1.50);
-    auto material_bubble = make_shared<dielectric>(1.50);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.6);
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0., -1000., 0.), 1000., ground_material));
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    float square_size = 5.;
+    float max_2d_radius = .6; // Max random radius around point x, z in loop
+    float min_radius = 0.3;
+    float max_radius = 0.55;
+
+    // X and Z coordinates on 2D plane
+    for(float x = -square_size/2.; x < square_size/2.; x++)
+    {
+        for(int z = -square_size/2.; z < square_size/2.; z++)
+        {
+            // random number dictating which material is chosen
+            float material_random = random_double();
+
+            float radius = random_double(min_radius, max_radius) * max_radius;
+
+            point3 center = point3(
+                x + random_double()*max_2d_radius,
+                radius,
+                z + random_double()*max_2d_radius
+            );
+
+            shared_ptr<material> mat;
+
+            // Material selection
+            // Everything is pretty much random
+            if(material_random < 0.7)
+            {
+                mat = make_shared<lambertian>(color(random_double(), random_double(), random_double()));
+            } else
+            if(material_random < 0.9)
+            {
+                mat = make_shared<metal>(color(random_double(), random_double(), random_double()), random_double());
+            } else
+            {
+                mat = make_shared<dielectric>(1. + random_double());
+            }
+
+            world.add(make_shared<sphere>(center, radius, mat));
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.vertical_fov = 40.;
+    cam.image_width = 300;
+    cam.vertical_fov = 20.;
     cam.sample_per_pix = 50;
 
-    cam.max_ray_depth = 20;
-    cam.look_from = point3(-2,2,1);
-    cam.look_at   = point3(0,0,-1);
-    cam.v_up      = vec3(0,1,0);
+    cam.max_ray_depth = 5;
+    cam.look_from = point3(15, 2.9, 2.6);
+    cam.look_at   = point3(-0.3, 0.5, 0.);
+    cam.v_up      = vec3(0., 1., 0.);
 
-    cam.defocus_angle  = 5.0;
-    cam.focus_distance = 3.4;
+    cam.defocus_angle  = 1.0;
+    cam.focus_distance = (cam.look_from - cam.look_at).length();
 
     cam.render(world);
 
